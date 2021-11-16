71f0223db877b2972fe7326833639497a5a6d8f1
Jovana Miladinovic <mjovanaam@gmail.com>
2021-11-16 22:50:34 +0100

    Kreirana je prijava.php strana.

--- prijava.php
+++ prijava.php
@@ -16,6 +16,33 @@ class Prijava {
         $query="SELECT * FROM prijave";
         return $conn->query($query);
     }
+
+    public static function getbyid($id, mysqli $conn) {
+        $query="SELECT * FROM prijave WHERE id=$id";
+        $myArray=array();
+        $rezultat=$conn->query($query);
+        if($rezultat) {
+            while($row=$rezultat->fetch_array()) {
+                $myArray[]=$row;
+            }
+        }
+        return $myArray;
+    }
+
+    public function deletebyid(mysqli $conn) {
+        $query="DELETE FROM prijave WHERE id=$this->id";
+        return $conn->query($query);
+    }
+
+    public static function add(Prijava $prijava, mysqli $conn) {
+        $query="INSERT INTO prijave (kurs, profesor, cena) VALUES ('$prijava->kurs','$prijava->profesor', $prijava->cena)";
+        return $conn->query($query);
+    }
+
+    public function update(mysqli $conn) {
+        $query="UPDATE prijave set kurs='$this->kurs, profesor='$this->profesor', cena=$this->cena";
+        return $conn->query($query);
+    }
 }
 
 
