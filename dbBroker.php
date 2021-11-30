b899969d24cb9d50e6179e4046c894232026ceff
Jovana Miladinovic <mjovanaam@gmail.com>
2021-11-30 15:23:13 +0100

    Završena je dbBroker.php.

--- dbBroker.php
+++ dbBroker.php
@@ -6,8 +6,8 @@ $password="";
 
 $conn=new mysqli($host, $user, $password, $db);
 
+
 if($conn->connect_errno) {
     exit("Neuspesna konekcija: greska>".$conn->connect_error.", err kod>".$conn->connect_errno);
 }
-
 ?>
