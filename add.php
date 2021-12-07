fa8a2428188e131052a3ecd3051953682b708ddd
Jovana Miladinovic <mjovanaam@gmail.com>
2021-12-07 20:35:40 +0100

    Dodati su komentari.

--- add.php
+++ add.php
@@ -2,8 +2,9 @@
 require "dbBroker.php";
 require "prijava.php";
 
+//ako sam upisala ove vrednosti
 if(isset($_POST['course']) && isset($_POST['teacher']) && isset($_POST['price'])) {
-
+//one idu u prijavu
     $prijava=new Prijava(null, $_POST['course'], $_POST['teacher'], $_POST['price']);
     $status=Prijava::add($prijava, $conn);
     if($status) {
